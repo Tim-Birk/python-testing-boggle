a99ee58d4c7c30814989c5ba65a5e23ac0b022f3
Tim Birkmire <timbirkmire@yahoo.com>
2021-01-08 12:18:57 -0500

    Create inital route, create board template, guess form and initial styling

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from boggle import Boggle
-from flask import Flask
+from flask import Flask, render_template, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 boggle_game = Boggle()
 
-app = Flask(__app__)
+app = Flask(__name__)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def display_game():
+    board = boggle_game.make_board()
+    session['board'] = board
+    return render_template("board.html")
